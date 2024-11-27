977f2b16ff00375c4cafe7caea6a560c09b0261c
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-27 17:56:49 +0300

    feat: implement Slider functionality

--- christmas-shop/js/index.js
+++ christmas-shop/js/index.js
@@ -1,9 +1,13 @@
 import { CLASS_HEADER, Header } from './header.js';
 import './score.js';
+import { CLASS_SLIDER, sliderOptions, Slider } from './slider.js';
 import { getSelector } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll(getSelector(CLASS_HEADER)).forEach((element) => {
     new Header(element);
+  });
+  document.querySelectorAll(getSelector(CLASS_SLIDER)).forEach((element) => {
+    new Slider(element, sliderOptions);
   });
 });
