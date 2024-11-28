b61d94486ea58104f621fa4e3b80dd7d89a3bdeb
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-28 09:33:57 +0300

    feat: implement timer functionality

--- christmas-shop/js/index.js
+++ christmas-shop/js/index.js
@@ -1,6 +1,7 @@
 import { CLASS_HEADER, Header } from './header.js';
 import './score.js';
 import { CLASS_SLIDER, sliderOptions, Slider } from './slider.js';
+import { CLASS_TIMER, TIMER } from './timer.js';
 import { getSelector } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -9,5 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   document.querySelectorAll(getSelector(CLASS_SLIDER)).forEach((element) => {
     new Slider(element, sliderOptions);
+  });
+  document.querySelectorAll(getSelector(CLASS_TIMER)).forEach((element) => {
+    new TIMER(element);
   });
 });
